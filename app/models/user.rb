d8a9d3f715f293254123b50e17b060510661cdb8
ZjanPreijde <j.preijde2@upcmail.nl>
2018-01-18 13:51:49 +0100

    Getting desperate ... (continued)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,7 @@ class User < ApplicationRecord
   has_many :events, dependent: :destroy
   has_one :profile, dependent: :destroy
 
-  # Validations
-  validates :email, presence: true
-  validates :password, presence: true
-
-  # Include default devise modules. Others available are:
+# Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 
-  # resources :users do
-  #     resources :events
-  # end
   resources :events, except: [:destroy]
   resources :profiles, only: [:new, :edit, :create, :update]
 
@@ -12,8 +9,4 @@ Rails.application.routes.draw do
 
   get "about" => "pages#about"
   get "contact" => "pages#contact"
-
-  # resources :events do
-  #   resources :registrations, only: [:create]
-  # end
 end
